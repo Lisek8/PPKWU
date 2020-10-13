c0ef945397a3a9e49948930c115a6d4a01664e03
Lisek8 <fafik989@gmail.com>
2020-10-13 15:05:27 +0200

    Added endpoint for reversing string

--- Zadanie1/main.js
+++ Zadanie1/main.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const port = 8000;
 const restServer = express();
+
+/*
+    Request/response format (JSON)
+    {
+        "string": <value>
+    }
+*/
+restServer.put('/', bodyParser.json(), (request, response) => {
+    try {
+        return response.send({ string: request.body.string.split("").reverse().join("") });
+    } catch (error) {
+        response.status = 400;
+        return response.send();
+    }
+});
+
 
 restServer.listen(port, () => {
     console.log(`Server listening on port ${port}.`);

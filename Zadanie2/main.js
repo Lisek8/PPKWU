2e37d90666b3aecac4872ffd43f113a4ac0d251e
Lisek8 <fafik989@gmail.com>
2020-10-27 14:22:27 +0100

    [Zad2] Created simple rest server

---
+++ Zadanie2/main.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = "8080";
+const restServer = express();
+
+restServer.listen(port, () => {
+    console.log(`Server listening on port ${port}.`);
+});

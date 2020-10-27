95ebb1a5fce34bd67fd0b47726a24940314fd7c9
Lisek8 <fafik989@gmail.com>
2020-10-27 15:09:27 +0100

    [Zad2] Implementation of functionality for /analyzeString endpoint

--- Zadanie2/main.js
+++ Zadanie2/main.js
@@ -4,8 +4,42 @@ const bodyParser = require('body-parser');
 const port = "8080";
 const restServer = express();
 
+function analyzeString(stringToAnalyze, responseBody) {
+    for (let char of stringToAnalyze.split("")) {
+        if (char.toLowerCase() !== char.toUpperCase()) {
+            if (char === char.toUpperCase()) {
+                responseBody.uppercaseCount += 1;
+            } else {
+                responseBody.lowercaseCount += 1;
+            }
+            continue;
+        }
+        
+        if (isNaN(parseInt(char))) {
+            responseBody.specialCount += 1;
+            responseBody.specialUsed.push(char);
+        } else {
+            responseBody.numberCount += 1;
+        }
+    }
+    responseBody.specialUsed = [... new Set(responseBody.specialUsed)];
+}
+
 restServer.put('/analyzeString', bodyParser.json(), (request, response) => {
-    return response.status(200).send();
+    const responseBody = {
+        lowercaseCount: 0,
+        uppercaseCount: 0,
+        numberCount: 0,
+        specialCount: 0,
+        specialUsed: []
+    };
+    try {
+        analyzeString(request.body.string, responseBody);
+    } catch (error) {
+        response.status(400).json({ error: error.toString() });
+        return response.send();
+    }
+    return response.send(responseBody);
 });
 
 restServer.listen(port, () => {

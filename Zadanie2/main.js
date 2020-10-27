83cce1a12274ccf0398063cc32fa8ebfdc2af7f7
Lisek8 <fafik989@gmail.com>
2020-10-27 14:26:45 +0100

    [Zad2] Added analyzeString endpoint

--- Zadanie2/main.js
+++ Zadanie2/main.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const port = "8080";
 const restServer = express();
 
+restServer.put('/analyzeString', bodyParser.json(), (request, response) => {
+    return response.status(200).send();
+});
+
 restServer.listen(port, () => {
     console.log(`Server listening on port ${port}.`);
 });

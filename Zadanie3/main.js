86953db21c21011de27d473df68c9b353b277391
Lisek8 <fafik989@gmail.com>
2020-11-10 15:36:31 +0100

    [Zad3] Added example events and tested if it works

--- Zadanie3/main.js
+++ Zadanie3/main.js
@@ -5,6 +5,19 @@ function icsDataGenerator(calendarData) {
   icsData.push("BEGIN:VCALENDAR");
   icsData.push("VERSION:2.0");
   icsData.push("PRODID://21599//PL");
+
+  // Example events for testing purposes
+  icsData.push("BEGIN:VEVENT");
+  icsData.push("DTSTART:20201110T150000Z");
+  icsData.push("DTEND:20201110T160000Z");
+  icsData.push("SUMMARY:Example event");
+  icsData.push("END:VEVENT");
+
+  icsData.push("BEGIN:VEVENT");
+  icsData.push("DTSTART:20201110T150000Z");
+  icsData.push("DTEND:20201110T160000Z");
+  icsData.push("SUMMARY:Example event2");
+  icsData.push("END:VEVENT");
   
   icsData.push("END:VCALENDAR");
   fs.writeFileSync("output.ics", icsData.join('\n'));

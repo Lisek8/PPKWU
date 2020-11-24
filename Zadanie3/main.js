615609e621a1487b6177a839262c69d9ac3e73c9
Lisek8 <fafik989@gmail.com>
2020-11-24 14:38:13 +0100

    [Zad3] Added simple endpoint that's supposed to return data straight from api

--- Zadanie3/main.js
+++ Zadanie3/main.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const http = require('http');
 
 const port = "8080";
 const restServer = express();
+const weeiaCalendarUrl = 'http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php';
 
 function virtulCalendarDataParser(calendarData) {
   
@@ -31,6 +33,28 @@ function icsDataGenerator(calendarData) {
   icsData.push("END:VCALENDAR");
   fs.writeFileSync("output.ics", icsData.join('\n'));
 }
+
+restServer.get('/calendar', bodyParser.json(), async (request, response) => {
+  const year = '2020';
+  const month = '03';
+  let apiData = '';
+  await http.get(weeiaCalendarUrl + `?rok=${year}&miesiac=${month}`, (apiResponse) => {
+    let data = '';
+
+    apiResponse.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    apiResponse.on('end', () => {
+      apiData = data;
+      console.log(data);
+    });
+  }).on('error', () => {});
+
+  response.writeHead(200, { 'Content-Type':'text/html'});
+  response.write(apiData);
+  response.end();
+});
 
 restServer.listen(port, () => {
   console.log(`Server listening on port ${port}.`);

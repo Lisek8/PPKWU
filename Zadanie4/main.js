a4b7803eeec9f4bee6ff0b384984db154fbe8141
Lisek8 <fafik989@gmail.com>
2020-12-08 14:37:56 +0100

    [Zad4] Added required libs and prepared /search enpoint for implementation

--- Zadanie4/main.js
+++ Zadanie4/main.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const requestPromise = require('request-promise-native');
+const HTMLParser = require('node-html-parser');
 
 const port = '8080';
 const restServer = express();
+const panoramaFirmSearchUrl = 'https://panoramafirm.pl/szukaj';
+
+restServer.get('/search', bodyParser.json(), async (request, response) => {
+  
+});
 
 restServer.listen(port, () => {
   console.log(`Server listening on port ${port}.`);

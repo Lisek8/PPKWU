e1870ebf0573793b12793647717ea8ef8581f82f
Lisek8 <fafik989@gmail.com>
2020-12-08 14:51:34 +0100

    [Zad4] /search endpoint requests data from api

--- Zadanie4/main.js
+++ Zadanie4/main.js
@@ -8,7 +8,11 @@ const restServer = express();
 const panoramaFirmSearchUrl = 'https://panoramafirm.pl/szukaj';
 
 restServer.get('/search', bodyParser.json(), async (request, response) => {
-  
+  const apiData = await requestPromise({
+    method: 'GET',
+    uri: panoramaFirmSearchUrl + '?k=' + escape('hydraulik') + '&l=' + escape('łódź')
+  });
+  console.log(apiData);
 });
 
 restServer.listen(port, () => {
